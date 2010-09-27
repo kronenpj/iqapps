04ea167a37f934fa4462daf2cb00cd97afbe166a
kronenpj <kronenpj@5b4f4ec6-0c85-11df-9e6e-d17b0969bdea>
2010-09-27 23:35:13 +0000

    * Rearranged methods.
    git-svn-id: https://iqapps.googlecode.com/svn/trunk@61 5b4f4ec6-0c85-11df-9e6e-d17b0969bdea

--- NWSAlerts/src/com/googlecode/iqapps/GetRequest.java
+++ NWSAlerts/src/com/googlecode/iqapps/GetRequest.java
@@ -16,24 +16,35 @@
 package com.googlecode.iqapps;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 public class GetRequest {
+	private static final Logger logger = Logger.getLogger("GetRequest");
+
 	static public String getRequest(String url) {
+		return HttpHelper.request(getResponse(url));
+	}
+
+	static public byte[] getRequestBytes(String url) {
+		return HttpHelper.requestBytes(getResponse(url));
+	}
+
+	static private HttpResponse getResponse(String url) {
 		HttpClient client = new DefaultHttpClient();
 		HttpGet request = new HttpGet(url);
 		HttpResponse response = null;
 		try {
 			response = client.execute(request);
 		} catch (Exception ex) {
+			logger.error("getResponse: " + ex.toString());
 			return null;
 		}
-		return HttpHelper.request(response);
-	}
-
-	static public byte[] getRequestBytes(String url) {
-		return getRequest(url).getBytes();
+		StatusLine statusLine = response.getStatusLine();
+		logger.debug("getResponse:  status: " + statusLine.getStatusCode());
+		logger.debug("getResponse:  reason: " + statusLine.getReasonPhrase());
+		return response;
 	}
 }
