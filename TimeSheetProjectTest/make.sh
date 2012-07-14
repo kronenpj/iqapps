1076136e54303afcf205b93eeaf39ff19765fc2c
Paul Kronenwetter <kronenpj@gmail.com>
2012-07-14 19:22:47 -0400

    Trying again to remove outdated testing directories.

--- TimeSheetProjectTest/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-AND_SERIAL="emulator-5554"
-TARGET="android-7"
-AND_HOME="/opt/android-sdk-linux_x86/platforms/$TARGET"
-
-BASE=$(/bin/ls -1 /opt | grep android | head)
-
-ANDROID_SERIAL="$AND_SERIAL" ANDROID_HOME="$AND_HOME" ant $*
